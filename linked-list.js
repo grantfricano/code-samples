fe04487b526fae23d96a6a888c65bf356e375f86
Grant Alexander <grant.fricano@gmail.com>
2022-08-09 13:36:16 -0400

    added some stuff to code

--- linked-list.js
+++ linked-list.js
@@ -31,10 +31,38 @@ class LinkedList {
         }
         this.size++;
     }
+
+    insertAt(index, element) {
+
+        var current, prev;
+        var i = 0;
+        var node = new Node(element);
+        current = this.head;
+
+        if (index == 0) {
+            node.next = this.head
+            this.head = node;
+        }
+        else {
+            while (i < index) { 
+                prev = current;
+                current = current.next;
+                i++;
+            }
+            node.next = current;
+            prev.next = node;
+        }
+        this.size++;
+    }
+
 };
 
 var list = new LinkedList;
 list.add(1);
+console.log(list);
 list.add(2);
+console.log(list);
 list.add(3);
+console.log(list);
+list.insertAt(0,10);
 console.log(list);
