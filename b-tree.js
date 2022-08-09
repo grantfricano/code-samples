0f250f0496c3d8f70668a430a0cdc60043a305a9
Grant Alexander <grant.fricano@gmail.com>
2022-08-09 14:00:03 -0400

    wrote a little b tree insert

---
+++ b-tree.js
@@ -0,0 +1,45 @@
+class TNode {
+
+    constructor(element) {
+        this.element = element;
+        this.left = null;
+        this.right = null;
+    }
+};
+
+class BTree {
+
+    constructor() {
+        this.root = null;
+    }
+
+    insert(element) {
+
+        if (this.root == null) {
+            this.root = new TNode(element);
+        }
+        else {
+            insertNode(this.root, new TNode(element))
+        }
+    }
+
+    insertNode(node, newNode) {
+
+        if (newNode.element < node.element){
+            if (node.left == null){
+                node.left = newNode;
+            }
+            else {
+                this.insertNode(node.left, newNode)
+            }
+        }
+        else {
+            if (node.right == null) {
+                node.right = newNode;
+            }
+            else {
+                this.insertNode(node.right, newNode)
+            }
+        }
+    }
+}
