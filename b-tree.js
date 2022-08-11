7ca39f94638131c681d424ee981bfbba7889d8a7
Grant Alexander <grant.fricano@gmail.com>
2022-08-11 12:54:24 -0400

    added recursive print for b tree

--- b-tree.js
+++ b-tree.js
@@ -24,7 +24,7 @@ class BTree {
     }
 
     insertNode(node, newNode) {
-        
+
         if (newNode.element < node.element){
             if (node.left == null){
                 node.left = newNode;
@@ -42,9 +42,43 @@ class BTree {
             }
         }
     }
+
+    print() {
+
+        this.printTree(this.root);
+
+    }
+
+    printTree(node) {
+        
+        if (node.left == null && node.right == null){
+            console.log(node.element);
+            return;
+        }
+        else if (node.left == null && node.right != null){
+            console.log(node.element);
+            this.printTree(node.right);
+        }
+        else if (node.left != null && node.right == null) {
+            this.printTree(node.left);
+            console.log(node.element);
+        }
+        else if (node.left != null && node.right != null) {
+            this.printTree(node.left);
+            console.log(node.element);
+            this.printTree(node.right);
+        }
+    }
 }
 
 var bTree = new BTree();
 bTree.insert(5);
 bTree.insert(10);
+bTree.insert(4);
+bTree.insert(1);
+bTree.insert(20);
 bTree.insert(3);
+bTree.insert(7);
+bTree.insert(15);
+bTree.insert(2);
+bTree.print();
