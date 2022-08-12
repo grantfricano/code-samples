2e157b46999f23dca39e90ce58a4dc877ba3f01c
Grant Alexander <grantalexander@argead.mynetworksettings.com>
2022-08-12 19:55:58 -0400

    lonely integer function

--- hacker-rank.js
+++ hacker-rank.js
@@ -42,3 +42,23 @@ function plusMinus(arr) {
     console.log((zeroNums/arrSize).toPrecision(6));
 
 }
+
+//input a = [1,2,3,4,3,2,1]
+function lonelyinteger(a) {
+    // Write your code here
+    const map = new Map();
+    for (let i = 0; i < a.length; i++) {
+        if (!map.get(a[i])){
+            map.set(a[i], 1);
+        }
+        else {
+            map.set(a[i], map.get(a[i]) + 1)
+        }
+    }
+    
+    for (let i = 0; i < a.length; i++) {
+        if (map.get(a[i]) == 1){
+            return a[i];
+        }
+    }
+}
