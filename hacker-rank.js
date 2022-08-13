43bace674a044245b421f54a042b5f8f5181d90a
Grant Alexander <grantalexander@argead.mynetworksettings.com>
2022-08-13 10:40:26 -0400

    countingSort algo

--- hacker-rank.js
+++ hacker-rank.js
@@ -85,3 +85,12 @@ function diagonalDifference(arr) {
     }
     return Math.abs(primary - secondary);
 }
+
+function countingSort(arr) {
+    // Write your code here
+    let countArr = Array(100).fill(0);
+    for (let i = 0; i < arr.length; i++){
+        countArr[arr[i]] = countArr[arr[i]] + 1;
+    }
+    return countArr;
+}
